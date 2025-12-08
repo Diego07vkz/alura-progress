05b0ff8b8159608b072988ecce0475b21338d276
Diego07vkz <diihdev07@gmail.com>
2025-12-08 16:42:07 -0300

    Organização geral do repositório

---
+++ alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo _1_iniciando_com_js/logica-js-projeto_inicial/desafios/desafio 01.js
@@ -0,0 +1,23 @@
+// DESAFIO 
+
+// 01. Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert('Bem-vindo ao nosso site');
+
+// 02. Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = 'Lua';
+
+// 03. Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+
+// 04. Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
+
+// 05. Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+
+// 06. Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert('Erro! Preencha todos os campos');
+
+// 07. Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
