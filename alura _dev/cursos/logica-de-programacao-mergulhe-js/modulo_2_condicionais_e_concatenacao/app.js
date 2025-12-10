81fec604fbb185af008b861b63b8b70ce70ccf54
Diego07vkz <diihdev07@gmail.com>
2025-12-10 15:30:38 -0300

    Adicionando arquivos do módulo 2, incluindo lógica do jogo do número secreto em JavaScript, estrutura HTML e estilos CSS. - concatenação

---
+++ alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo_2_condicionais_e_concatenacao/app.js
@@ -0,0 +1,13 @@
+// esse código tem a mesma ideia do modulo anterior, muda apenas algumas funções que aprendi, como: condicionais if e else
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 7; 
+console.log(numeroSecreto)
+let chute = (prompt('Digite um número entre 1 e 10'));
+
+
+// se chute for igual ao número secreto 
+if (chute == numeroSecreto) {
+    alert(`Parabéns! Você acertou o numero secreto!${numeroSecreto}`);
+} else {
+    alert('Você errou')
+}
