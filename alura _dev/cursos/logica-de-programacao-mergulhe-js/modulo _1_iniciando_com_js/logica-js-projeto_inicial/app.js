84e17d3d57aec64cd8dfcb2f48a7695e9ba112a4
Diego07vkz <diihdev07@gmail.com>
2025-12-10 14:30:18 -0300

    exclui sem querer o conteúdo do app.js, na hora de organizar, mas já reescrevi novamente

---
+++ alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo _1_iniciando_com_js/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+// eu tinha feito essa parte, mas na hora de organizar pq tava tudo jun to, acabei apagando sem querer kkkk
+
+alert('Boas-vindas ao jogo do número secreto');
+let numeroSecreto = 7; 
+let chute = (prompt('Digite um número entre 1 e 10'));
+
+
+// se chute for igual ao número secreto 
+if (chute == numeroSecreto) {
+    alert('Parabéns! Você acertou o numero secreto!')
+}
