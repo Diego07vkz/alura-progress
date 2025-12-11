17d176bc723d6bb58fe166c0ab224ad2991ef323
Diego07vkz <diihdev07@gmail.com>
2025-12-10 21:33:01 -0300

    Organizando e adicionando arquivos do desafio 01 e 02, incluindo lógica em JavaScript e estrutura HTML.

---
+++ alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo_2_condicionais_e_concatenacao/desafios/desafio02.js/app.js
@@ -0,0 +1,11 @@
+// aqui eu tinha que apenas verificar se o número digitado era positivo ou negativo, foi bem fácil
+
+alert('Seja bem-vindo ao Desafio 02 ');
+let num = prompt('Digite um número');
+
+    if (num >= 0) {
+        alert('Número positivo');
+    }
+    else { 
+        alert('Número negativo');
+    }
