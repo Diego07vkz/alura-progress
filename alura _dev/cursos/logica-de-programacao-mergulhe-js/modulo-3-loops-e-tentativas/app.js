a289cab53234d5430ff8e8fbbaafda6381cc9c46
Diego07vkz <diihdev07@gmail.com>
2025-12-15 11:36:00 -0300

    Refatorando lógica do jogo do número secreto para utilizar loop while e corrigindo a inicialização da variável chute

--- alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo-3-loops-e-tentativas/app.js
+++ alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo-3-loops-e-tentativas/app.js
@@ -2,16 +2,20 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 7; 
 console.log(numeroSecreto)
-let chute = (prompt('Digite um número entre 1 e 10'));
+let chute 
 
-
-// se chute for igual ao número secreto 
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o numero secreto! ( ${numeroSecreto} )`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`Você errou! O número secreto é menor que ${chute}`);
+// Introduzindo loop while
+while (chute != numeroSecreto) {
+    chute = prompt('Digite um número entre 1 e 10');
+// se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você acertou o numero secreto! ( ${numeroSecreto} )`);
     } else {
-        alert(`Você errou! O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`Você errou! O número secreto é menor que ${chute}`);
+        } else {
+            alert(`Você errou! O número secreto é maior que ${chute}`);
+        }
     }
 }
+
--- alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo_2_condicionais_e_concatenacao/app.js
+++ alura _dev/cursos/logica-de-programacao-mergulhe-js/modulo_2_condicionais_e_concatenacao/app.js
@@ -2,7 +2,7 @@
 alert('Boas-vindas ao jogo do número secreto');
 let numeroSecreto = 7; 
 console.log(numeroSecreto)
-let chute = (prompt('Digite um número entre 1 e 10'));
+let chute = prompt('Digite um número entre 1 e 10');
 
 
 // se chute for igual ao número secreto 
